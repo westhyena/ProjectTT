3191571b2eaa8f3485507782bf604b1df13db894
Jungwoo <westhyena@gmail.com>
2023-12-25 17:56:38 +0900

    feat: player walk animation & scale

--- Assets/Scripts/InGame/Object/Player.cs
+++ Assets/Scripts/InGame/Object/Player.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator animator;
     public float movementSpeed = 20.0f;
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +26,21 @@ public class Player : MonoBehaviour
 
     public void Move(Vector2 movement)
     {
-        transform.position += new Vector3(
+        float absSpeed = Mathf.Max
+        (
+            Mathf.Abs(movement.x),
+            Mathf.Abs(movement.y)
+        );
+        animator.SetFloat("speed", absSpeed);
+
+        transform.localScale = new Vector3(
+            -Mathf.Sign(movement.x),
+            1.0f,
+            1.0f
+        );
+
+        transform.position += new Vector3
+        (
             movementSpeed * movement.x * Time.deltaTime,
             movementSpeed * movement.y * Time.deltaTime,
             0.0f
