e92f9dfaf1f4b7878e1d52bb98a3336680eb15ac
Jungwoo <westhyena@gmail.com>
2024-02-09 16:34:02 +0900

    시간지나면서 wave 종료

--- Assets/Scripts/InGame/Manager/WaveManager.cs
+++ Assets/Scripts/InGame/Manager/WaveManager.cs
@@ -58,9 +58,31 @@ public class WaveManager : MonoBehaviour
     public class WaveInfo
     {
         public List<WaveMonsterInfo> waveMonsterInfoList;
-        public WaveInfo(WaveMonsterInfo[] waveMonsterInfos)
+        float waveEndTime = 60.0f;
+        float waveTimer = 0.0f;
+
+        public WaveInfo(WaveMonsterInfo[] waveMonsterInfos, float waveEndTime)
         {
             waveMonsterInfoList = new List<WaveMonsterInfo>(waveMonsterInfos);
+            this.waveEndTime = waveEndTime;
+        }
+
+        public bool Update(float deltaTime, EnemyManager enemyManager, Transform[] spawnPoints)
+        {
+            bool isSpawnEnd = true;
+            foreach (WaveMonsterInfo waveMonsterInfo in waveMonsterInfoList)
+            {
+                bool eachEnd = waveMonsterInfo.Update(deltaTime, enemyManager, spawnPoints);
+                if (!eachEnd)
+                {
+                    isSpawnEnd = false;
+                }
+            }
+
+            return (
+                (isSpawnEnd && enemyManager.AliveEnemyList.Count == 0)
+                || waveTimer > waveEndTime
+            );
         }
     }
 
@@ -70,7 +92,6 @@ public class WaveManager : MonoBehaviour
     readonly List<WaveInfo> waveInfoList = new();
 
     float totalTimer = 0.0f;
-    float waveTimer = 0.0f;
 
     int curWaveIndex = 0;
 
@@ -79,39 +100,32 @@ public class WaveManager : MonoBehaviour
         enemyManager = GetComponent<EnemyManager>();
         waveInfoList.Add(new WaveInfo(new WaveMonsterInfo[] {
             new (0, 1.0f, 2.0f, 10, 10),
-        }));
+        }, 60.0f));
         waveInfoList.Add(new WaveInfo(new WaveMonsterInfo[] {
             new (0, 1.0f, 2.0f, 20, 10),
-        }));
+        }, 60.0f));
         waveInfoList.Add(new WaveInfo(new WaveMonsterInfo[] {
             new(0, 1.0f, 2.0f, 30, 10),
-        }));
+        }, 60.0f));
         
     }
 
     void CheckWave()
     {
-        WaveInfo curWaveInfo = waveInfoList[curWaveIndex];
-        bool isSpawnEnd = true;
-        foreach (WaveMonsterInfo waveMonsterInfo in curWaveInfo.waveMonsterInfoList)
+        if (curWaveIndex >= waveInfoList.Count)
         {
-            bool eachEnd = waveMonsterInfo.Update(Time.deltaTime, enemyManager, spawnPoints);
-            if (!eachEnd)
-            {
-                isSpawnEnd = false;
-            }
+            return;
         }
-
-        if (isSpawnEnd && enemyManager.AliveEnemyList.Count == 0)
+        WaveInfo curWaveInfo = waveInfoList[curWaveIndex];
+        bool waveEnd = curWaveInfo.Update(Time.deltaTime, enemyManager, spawnPoints);
+        if (waveEnd)
         {
             curWaveIndex++;
-            waveTimer = 0.0f;
         }
     }
 
     void Update()
     {
-        waveTimer += Time.deltaTime;
         totalTimer += Time.deltaTime;
         CheckWave();
     }
