724bd002682f315135073b2cb380ca9156dc1a29
Jungwoo <westhyena@gmail.com>
2024-03-24 18:09:04 +0900

    Stage 데이터

--- Assets/Scripts/Editor/DataConverter.cs
+++ Assets/Scripts/Editor/DataConverter.cs
@@ -1,6 +1,4 @@
 #if UNITY_EDITOR
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -17,6 +15,7 @@ public class DataConverter : MonoBehaviour
     private static string NOTION_VERSION = "2022-06-28";
 
     private static string CHARACTER_INFO_DATABASE = "7e9bd778c8d340a485aa7a502eb28544";
+    private static string WAVE_GROUP_INFO_DATABASE = "9d942e801ef04543a5924b560ca28416";
     private static string STAGE_INFO_DATABASE = "c94148b20149490ab913824bf675c88a";
 
     [MenuItem("Data Manager/Download")]
@@ -71,6 +70,17 @@ public class DataConverter : MonoBehaviour
         int.TryParse(intToken.ToString(), out int intValue);
         return intValue;
     }
+
+    static string GetRelationUUID(JObject propertyObj, string key)
+    {
+        JObject obj = propertyObj.GetValue(key) as JObject;
+        JArray typeAry = obj.GetValue("relation") as JArray;
+        if (typeAry.Count == 0) return null;
+
+        JObject typeObj = typeAry[0] as JObject;
+        return typeObj.GetValue("id").ToString();
+    }
+
     static JArray DownloadNotionDatabase(string databaseId)
     {
         string url = "https://api.notion.com/v1/databases/" + databaseId + "/query";
@@ -89,6 +99,22 @@ public class DataConverter : MonoBehaviour
         JObject data = JObject.Parse(request.downloadHandler.text);
         JArray results = data.GetValue("results") as JArray;
         return results;
+    }
+
+    static Dictionary<string, string> GetUUIDDictionary(string databaseId)
+    {
+        JArray results = DownloadNotionDatabase(databaseId);
+        Dictionary<string, string> uuidDict = new();
+        foreach (JObject obj in results.Cast<JObject>())
+        {
+            JObject propertyObj = obj.GetValue("properties") as JObject;
+            string id = GetString(propertyObj, "id");
+            if (id == null) continue;
+
+            string uuid = obj.GetValue("id").ToString();
+            uuidDict[uuid] = id;
+        }
+        return uuidDict;
     }
 
     static void DownloadCharacterData()
@@ -123,19 +149,50 @@ public class DataConverter : MonoBehaviour
     static void DownloadStageData()
     {
         JArray results = DownloadNotionDatabase(STAGE_INFO_DATABASE);
+        Dictionary<string, string> waveGroupDict = GetUUIDDictionary(WAVE_GROUP_INFO_DATABASE);
         JArray ary = new();
         foreach (JObject obj in results.Cast<JObject>())
         {
             JObject newObj = new();
-            Debug.Log(obj);
             
             JObject propertyObj = obj.GetValue("properties") as JObject;
             string id = GetString(propertyObj, "id");
             if (id == null) continue;
 
+            string phase01waveGroupUUID = GetRelationUUID(propertyObj, "phase01waveGroup");
+            string phase02waveGroupUUID = GetRelationUUID(propertyObj, "phase02waveGroup");
+            string phase03waveGroupUUID = GetRelationUUID(propertyObj, "phase03waveGroup");
+            string phase04waveGroupUUID = GetRelationUUID(propertyObj, "phase04waveGroup");
+            string phase05waveGroupUUID = GetRelationUUID(propertyObj, "phase05waveGroup");
+
             newObj.Add("id", id);
-            Debug.Log(propertyObj.GetValue("phase01waveGroup"));
+            newObj.Add(
+                "phase01waveGroup",
+                waveGroupDict.GetValueOrDefault(phase01waveGroupUUID, null)
+            );
+            newObj.Add(
+                "phase02waveGroup",
+                waveGroupDict.GetValueOrDefault(phase02waveGroupUUID, null)
+            );
+            newObj.Add(
+                "phase03waveGroup",
+                waveGroupDict.GetValueOrDefault(phase03waveGroupUUID, null)
+            );
+            newObj.Add(
+                "phase04waveGroup",
+                waveGroupDict.GetValueOrDefault(phase04waveGroupUUID, null)
+            );
+            newObj.Add(
+                "phase05waveGroup",
+                waveGroupDict.GetValueOrDefault(phase05waveGroupUUID, null)
+            );
+
+            newObj.Add("stagePrefab", GetString(propertyObj, "stagePrefab"));
+
+            ary.Add(newObj);
         }
+        string path = Path.Combine(Application.streamingAssetsPath, "StageData.csv");
+        ConvertJArrayToCSV(ary, path);
     }
 }
 #endif
