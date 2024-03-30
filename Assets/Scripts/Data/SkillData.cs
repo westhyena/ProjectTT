adc48d7fb378eb97f049b2a20d29dd5f8f3161e2
Jungwoo <westhyena@gmail.com>
2024-03-30 15:10:10 +0900

    SkillData 다운로드&불러오기

---
+++ Assets/Scripts/Data/SkillData.cs
@@ -0,0 +1,74 @@
+using System;
+
+[Serializable]
+public class SkillData
+{
+    public SkillInfo[] items;
+    public SkillData(string[,] csvGrid)
+    {
+        items = new SkillInfo[csvGrid.GetUpperBound(1) - 1];
+        for (int i = 0; i < items.Length; ++i)
+        {
+            try
+            {
+                items[i] = new SkillInfo(csvGrid, i + 1);
+            }
+            catch (Exception)
+            {
+                items[i] = null;
+            }
+        }
+    }    
+}
+
+[Serializable]
+public class SkillInfo
+{
+    public string id;
+    public string memo;
+    public string skillAttribute;
+    public bool isRangeAttack;
+    public float coolDown;
+    public string target;
+    public float rangeOfSkill;
+    public string projectileID;
+    public string skillEffects01;
+    public float effectsValue01;
+    public float duration01;
+    public string skillEffects02;
+    public float effectsValue02;
+    public float duration02;
+    public string skillEffects03;
+    public float effectsValue03;
+    public float duration03;
+    public string atkAnimation;
+    public string atkedEffect;
+    public string rangeEffect;
+    public string iconSprite;
+    
+    public SkillInfo(string[,] csvGrid, int rowIdx)
+    {
+        int idx = 0;
+        id = csvGrid[idx++, rowIdx];
+        memo = csvGrid[idx++, rowIdx];
+        skillAttribute = csvGrid[idx++, rowIdx];
+        isRangeAttack = bool.Parse(csvGrid[idx++, rowIdx]);
+        coolDown = float.Parse(csvGrid[idx++, rowIdx]);
+        target = csvGrid[idx++, rowIdx];
+        rangeOfSkill = float.Parse(csvGrid[idx++, rowIdx]);
+        projectileID = csvGrid[idx++, rowIdx];
+        skillEffects01 = csvGrid[idx++, rowIdx];
+        effectsValue01 = float.Parse(csvGrid[idx++, rowIdx]);
+        duration01 = float.Parse(csvGrid[idx++, rowIdx]);
+        skillEffects02 = csvGrid[idx++, rowIdx];
+        effectsValue02 = float.Parse(csvGrid[idx++, rowIdx]);
+        duration02 = float.Parse(csvGrid[idx++, rowIdx]);
+        skillEffects03 = csvGrid[idx++, rowIdx];
+        effectsValue03 = float.Parse(csvGrid[idx++, rowIdx]);
+        duration03 = float.Parse(csvGrid[idx++, rowIdx]);
+        atkAnimation = csvGrid[idx++, rowIdx];
+        atkedEffect = csvGrid[idx++, rowIdx];
+        rangeEffect = csvGrid[idx++, rowIdx];
+        iconSprite = csvGrid[idx++, rowIdx];
+    }
+}
