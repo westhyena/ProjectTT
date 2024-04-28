c5c25a6487ab738bcc033045ad366eba24d2300e
ljk <nameisljk@gmail.com>
2024-04-28 21:27:50 +0900

    변수명 수정

--- Assets/Scripts/Library/DataMgr.cs
+++ Assets/Scripts/Library/DataMgr.cs
@@ -796,14 +796,14 @@ public class DataMgr : MonoBehaviour
 	/// <summary>
 	/// 레벨에 따른 Grow수치를 적용한 수치 반환
 	/// </summary>
-	public void GetCharacterGrowData(int CharacterID,int CharacterLevel,ref int GrowHP,ref int GrowAttackDmg, ref int GrowPD, ref int GrowMD)
+	public void GetCharacterGrowData(int CharacterID,int CharacterLevel,ref int HP,ref int AttackDmg, ref int PD, ref int MD)
 	{
 		List<InGame_CharacterGrowData> TargetGrowDataList = GetGrowData(CharacterID);
 
-		GrowHP = (m_CharacterDataElementDic[CharacterID].GrowHp * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_Hp;
-		GrowAttackDmg = (m_CharacterDataElementDic[CharacterID].GrowHp * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_AttackDamage;
-		GrowPD = (m_CharacterDataElementDic[CharacterID].GrowPD * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_PD;
-		GrowMD = (m_CharacterDataElementDic[CharacterID].GrowMD * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_MD;
+		HP = (m_CharacterDataElementDic[CharacterID].GrowHp * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_Hp;
+		AttackDmg = (m_CharacterDataElementDic[CharacterID].GrowHp * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_AttackDamage;
+		PD = (m_CharacterDataElementDic[CharacterID].GrowPD * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_PD;
+		MD = (m_CharacterDataElementDic[CharacterID].GrowMD * CharacterLevel) + TargetGrowDataList[CharacterLevel].Add_MD;
 	}
 
 	/// <summary>
