be54c072bb12650aa0b3e94bcec4d4690a6dee15
Jungwoo <westhyena@gmail.com>
2024-05-15 09:51:26 +0900

    buffcard scene 세팅

--- Assets/Scripts/InGame/UI/BuffCardUI.cs
+++ Assets/Scripts/InGame/UI/BuffCardUI.cs
@@ -1,14 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class BuffCardUI : MonoBehaviour
 {
     public Button button;
 
+    public Image cardImage;
+
+    public Image iconImage;
+    public TMP_Text nameText;
+    public TMP_Text descriptionText;
+
     void Awake()
     {
         button = GetComponent<Button>();
+        cardImage = GetComponent<Image>();
     }
 }
