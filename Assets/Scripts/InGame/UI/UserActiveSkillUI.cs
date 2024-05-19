341fe1143b2333731f11189f526cd934ef85364a
Jungwoo <westhyena@gmail.com>
2024-05-19 20:58:06 +0900

    유저액티브스킬 신 셋팅

---
+++ Assets/Scripts/InGame/UI/UserActiveSkillUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserActiveSkillUI : MonoBehaviour
+{
+    [SerializeField]
+    UserActiveSkillSlotUI[] slotUIs;
+
+    void Awake()
+    {
+
+    }
+}
